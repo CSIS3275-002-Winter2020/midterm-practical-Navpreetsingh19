c0015e92aea63a3022b1d6344c94b307a22016da
300311853 <300311853@R5107S12.ad.douglascollege.ca>
2020-02-14 13:44:09 -0800

    Name  and email has been added

---
+++ Winter2020CSIS3275Midterm/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.print("Navpreet Singh");
+        System.out.print("singhn24@student.douglascollege.ca");
+        
+	}
+
+}
